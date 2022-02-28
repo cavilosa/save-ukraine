4576b4b6c3e703e1ca48b51df1568487cf600028
cavilosa <cavilosa33@gmail.com>
2022-02-28 10:44:01 -0700

    Added stuctural files.

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, request, abort, make_response, flash, jsonify,\
-make_response, redirect, request, url_for
+make_response, redirect, request, url_for, render_template
 from flask_caching import Cache
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-# cache = Cache(app)
-# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-
-# app = Flask(__name__)
-# cache.init_app(app)
 
 @app.route("/", methods = ["GET"])
 @cache.cached(timeout=10)
 def index():
-    return ("success: True")
+
+
+    return render_template("layouts/main.html")
 
 
 if __name__ == "__main__":
